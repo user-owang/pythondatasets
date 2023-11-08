939b9ed87fa0672e3085b754edf3efb1ccccf36d
unknown <hilow0090@gmail.com>
2023-11-08 13:26:28 -0800

    finished partition

--- 16_partition/partition.py
+++ 16_partition/partition.py
@@ -19,3 +19,6 @@ def partition(lst, fn):
         >>> partition(["hi", None, 6, "bye"], is_string)
         [['hi', 'bye'], [None, 6]]
     """
+    a = [item for item in lst if fn(item)]
+    b = [item for item in lst if not(fn(item))]
+    return [a,b]
