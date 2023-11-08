7ad47dd359e81a78666cf798e8c1b359da1fbe42
unknown <hilow0090@gmail.com>
2023-11-08 00:04:23 -0800

    finished list manipulation

--- 08_list_manipulation/list_manipulation.py
+++ 08_list_manipulation/list_manipulation.py
@@ -40,3 +40,15 @@ def list_manipulation(lst, command, location, value=None):
         >>> list_manipulation(lst, 'add', 'dunno') is None
         True
     """
+    if command == 'add' and location == 'end':
+        lst.append(value)
+        return lst
+    if command == 'add' and location == 'beginning':
+        lst.insert(0,value)
+        return lst
+    if command == 'remove':
+        if location == 'end':
+            return lst.pop()
+        if location == 'beginning':
+            return lst.pop(0)
+    return None
