792d8cd993b2317f81205d11823f0f51ad89b642
unknown <hilow0090@gmail.com>
2023-11-08 22:04:32 -0800

    finished find the duplicate

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,7 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    for num in set(nums):
+        if nums.count(num)>1:
+            return num
+    return None
