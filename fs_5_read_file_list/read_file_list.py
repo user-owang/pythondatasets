549e518124f697878ed0296f8816bca78ecc973a
unknown <hilow0090@gmail.com>
2023-11-08 23:31:07 -0800

    couldnt figure out read file list

--- fs_5_read_file_list/read_file_list.py
+++ fs_5_read_file_list/read_file_list.py
@@ -19,3 +19,4 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+    
