de6d625a8be70c2cce09c9f5cbfbbc4433ecbba1
unknown <hilow0090@gmail.com>
2023-11-08 22:50:46 -0800

    finished valid parentheses

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,11 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    if parens[len(parens)-1] == '(' or parens[0] == ')':
+        return False
+    if parens.count('(') != parens.count(')'):
+        return False
+    for index in range(1,len(parens)):
+        if parens[0:index].count(')') > parens[0:index].count('('):
+            return False
+    return True
