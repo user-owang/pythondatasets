31355d4a567b976a746324a4efdf49f77a068419
unknown <hilow0090@gmail.com>
2023-11-09 15:24:43 -0800

    finished read file list

--- fs_5_read_file_list/read_file_list.py
+++ fs_5_read_file_list/read_file_list.py
@@ -19,4 +19,6 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
-    
+    file = open(filename,'r')
+    for line in file:
+        print(f'- {line.strip()}')
